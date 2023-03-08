28362276a4d8ebfd778fed2aff877e286494fbd7
MeGallin <me@garyallin.uk>
2023-03-08 21:35:17 +0000

    minor heading adjustments

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -12,20 +12,26 @@ const HeaderComponent = () => {
             </li>
           </ul>
           <ul>
-            <NavLink
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/"
-              data-cy="header-home-link"
-            >
-              home
-            </NavLink>
-            <NavLink
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/about"
-              data-cy="header-about-link"
-            >
-              about
-            </NavLink>
+            <li>
+              <NavLink
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/"
+                data-cy="header-home-link"
+              >
+                home
+              </NavLink>
+            </li>
+            <li>
+              {' '}
+              <NavLink
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/about"
+                data-cy="header-about-link"
+              >
+                about
+              </NavLink>
+            </li>
+            <li></li>
           </ul>
         </nav>
       </header>
