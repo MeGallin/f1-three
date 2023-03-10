e00693a187d631ef2965161ccf4367508b2729d7
MeGallin <me@garyallin.uk>
2023-03-10 11:57:18 +0000

    race results

--- src/Components/RaceResults/RaceResultsComponent.jsx
+++ src/Components/RaceResults/RaceResultsComponent.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { raceResultsAction } from '../../Store/Actions/RaceResults';
 import './RaceResultsComponent.css';
+import moment from 'moment';
 
 import { raceResultsData } from '../../Utils/TempData/RaceResults';
 
@@ -12,8 +13,6 @@ const RaceResultsComponent = () => {
   // const raceResults = useSelector((state) => state.raceResults);
   // const { loading, error, success, raceResults: raceResultsData } = raceResults;
 
-  console.log('comp', raceResultsData);
-
   const handleYearInput = (e) => {
     setYear(e.target.value);
   };
@@ -22,6 +21,24 @@ const RaceResultsComponent = () => {
     // dispatch(raceResultsAction(year));
   };
 
+  const result = raceResultsData.reduce((acc, curr) => {
+    const winnerKey = `${curr.winner.abbr} ${curr.winner.firstname} ${curr.winner.lastname}`;
+    if (acc[winnerKey]) {
+      acc[winnerKey].wins += 1;
+    } else {
+      acc[winnerKey] = {
+        abbr: curr.winner.abbr,
+        firstname: curr.winner.firstname,
+        lastname: curr.winner.lastname,
+        wins: 1,
+      };
+    }
+    return acc;
+  }, {});
+  const winners = Object.values(result);
+
+  console.log(winners);
+
   return (
     <>
       {false ? 'Error component' : null}
@@ -29,7 +46,7 @@ const RaceResultsComponent = () => {
         '...loading'
       ) : (
         <>
-          <form onSubmit={handleForm}>
+          <form onSubmit={handleForm} className="results-form">
             <label htmlFor="year">
               Change the Year
               <input
@@ -42,15 +59,52 @@ const RaceResultsComponent = () => {
                 required
               />
             </label>
-
             <button type="submit">Submit</button>
           </form>
+
+          <div className="results-summary border-temp">
+            <h6>Summary for {year}</h6>
+            <div className="grid ">
+              {winners.map((wins) => (
+                <div key={wins.abbr}>
+                  <h6>
+                    {wins.firstname} {wins.lastname}
+                  </h6>
+                  <h5>
+                    {wins.wins}
+                    <sub>
+                      <small>wins</small>
+                    </sub>
+                  </h5>
+                </div>
+              ))}
+            </div>
+          </div>
+
           {raceResultsData?.map((result) => (
             <div key={result.grandPrix}>
               <article>
-                <header>Header</header>
-                Body{result.car}
-                <footer>Footer</footer>
+                <h4>
+                  {result.grandPrix}
+                  <sub>
+                    <small>GP</small>
+                  </sub>
+                </h4>
+
+                <h4>
+                  <sup>winner</sup> {result.winner.firstname}{' '}
+                  {result.winner.lastname}
+                </h4>
+                <h4>
+                  <sup>Team</sup> {result.car}
+                </h4>
+                <h5>
+                  <sup>Laps</sup> {result.laps}
+                </h5>
+
+                <p className="small-text">
+                  {moment(result.date).format('MMMM Do YYYY, h:mm a')}
+                </p>
               </article>
             </div>
           ))}
