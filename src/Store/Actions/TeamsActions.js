c89220a894442f17a37532992d9c39231adaf951
MeGallin <me@garyallin.uk>
2023-03-17 16:57:15 +0000

    team data fetch

--- src/Store/Actions/TeamsActions.js
+++ src/Store/Actions/TeamsActions.js
@@ -3,6 +3,9 @@ import {
   TEAMS_FAILURE,
   TEAMS_REQUEST,
   TEAMS_SUCCESS,
+  TEAM_FAILURE,
+  TEAM_REQUEST,
+  TEAM_SUCCESS,
 } from '../Constants/TeamsConstants';
 
 //GET: Race result
@@ -27,6 +30,36 @@ export const teamsAction = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: TEAMS_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+//GET: Race result
+export const teamAction = (team) => async (dispatch) => {
+  try {
+    dispatch({
+      type: TEAM_REQUEST,
+    });
+
+    const options = {
+      method: 'GET',
+      url: `https://fia-formula-1-championship-statistics.p.rapidapi.com/api/teams/details/${team}`,
+
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_X_RapidAPI_Key,
+        'X-RapidAPI-Host': process.env.REACT_APP_X_RapidAPI_Host,
+      },
+    };
+
+    const { data } = await axios.request(options);
+    dispatch({ type: TEAM_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: TEAM_FAILURE,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- src/Store/Reducers/TeamsReducers.js
+++ src/Store/Reducers/TeamsReducers.js
@@ -2,6 +2,9 @@ import {
   TEAMS_FAILURE,
   TEAMS_REQUEST,
   TEAMS_SUCCESS,
+  TEAM_FAILURE,
+  TEAM_REQUEST,
+  TEAM_SUCCESS,
 } from '../Constants/TeamsConstants';
 
 //GET:  TEAMS NAMES
@@ -16,6 +19,25 @@ export const teamsReducer = (state = {}, action) => {
         ...action.payload,
       };
     case TEAMS_FAILURE:
+      return { loading: false, error: action.payload };
+
+    default:
+      return { ...state };
+  }
+};
+
+//GET:  TEAMS NAMES
+export const teamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case TEAM_REQUEST:
+      return { loading: true };
+    case TEAM_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        ...action.payload,
+      };
+    case TEAM_FAILURE:
       return { loading: false, error: action.payload };
 
     default:
