0e4a4c993a483df2e0a827ddd35fc6c89a8e844b
MeGallin <me@garyallin.uk>
2023-03-21 13:59:39 +0000

    standing working

--- src/Components/DriversStandings/DriverStandingsComponent.jsx
+++ src/Components/DriversStandings/DriverStandingsComponent.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './DriverStandingsComponent.css';
+
+//Temp data
 import { driverStandings } from '../../Utils/TempData/drivers';
 
 import {
@@ -13,26 +16,28 @@ import {
   Legend,
   ResponsiveContainer,
 } from 'recharts';
+import { driverStandingsAction } from '../../Store/Actions/DriversActions';
 
 const DriverStandingsComponent = () => {
-  const {
-    loading,
-    title,
-    driverStandings: driverStandingsData,
-  } = driverStandings;
+  const dispatch = useDispatch();
 
-  const data = driverStandingsData?.map((standings) => {
-    console.log(standings);
-    if (standings?.pts > 0) {
-      return {
-        name: standings?.driver.abbr,
-        points: standings?.pts,
-      };
+  useEffect(() => {
+    dispatch(driverStandingsAction());
+  }, [dispatch]);
+
+  const driverStandings = useSelector((state) => state.driverStandings);
+  const { loading, error, MRData } = driverStandings;
+
+  const standingData = MRData?.StandingsTable?.StandingsLists?.flatMap(
+    (data) => data.DriverStandings,
+  ).filter((standingsFinish) => {
+    if (standingsFinish?.points > '0') {
+      return standingsFinish;
     }
+    return false;
   });
-  const filteredData = data.filter(
-    (item) => item !== null && item !== undefined,
-  );
+
+  console.log(standingData);
 
   const BarWithBorder = (borderHeight, borderColor) => {
     return (props) => {
@@ -63,21 +68,30 @@ const DriverStandingsComponent = () => {
   return (
     <>
       <fieldset className="fieldSet">
-        <legend>{title}</legend>
+        <legend>Championship Standings</legend>
 
         <div className="graph-wrapper">
           <div
             style={{
               width: '100%',
-              height: 360,
+              height: '360px',
               backgroundColor: 'black',
+              position: 'relative',
             }}
           >
             <ResponsiveContainer>
-              <BarChart data={filteredData}>
+              <BarChart data={standingData}>
                 <CartesianGrid strokeDasharray="0.3 3" />
-                <XAxis dataKey="name" tick={{ fill: 'orange' }} />
-                <YAxis tick={{ fill: 'orange' }} />
+                <XAxis dataKey="Driver.code" tick={{ fill: 'orange' }} />
+                <YAxis
+                  tick={{ fill: 'orange' }}
+                  label={{
+                    value: 'points',
+                    angle: -90,
+                    position: 'insideLeft',
+                  }}
+                  height={20}
+                />
                 <Tooltip />
                 <Legend verticalAlign="top" height={36} />
                 <Bar
